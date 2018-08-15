2ffbd39b78257a3b206c3538d2ae970c6de9a158
Woody NaDobhar <woodynadobhar@hotmail.com>
2018-08-14 20:51:20 -0700

    Added Facebook Analytics. Added larger version of logo.
    Signed-off-by: Woody NaDobhar <woodynadobhar@hotmail.com>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,6 +38,26 @@
 </head>
 
 <body class="skin-purple sidebar-mini">
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : {!! '\'' . env('FACEBOOK_CLIENT') . '\'' !!},
+      xfbml      : true,
+      version    : 'v3.1'
+    });
+  
+    FB.AppEvents.logPageView();
+  
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
 @if (Request::segment(1) == 'sparse')
 	<div id="page-content-wrapper">
 		<div class="container-fluid">
