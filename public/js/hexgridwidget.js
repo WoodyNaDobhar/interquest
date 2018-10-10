c08543fc5616b85adb83ef3d9bca90e0aa2b3da9
Woody NaDobhar <woodynadobhar@hotmail.com>
2018-10-09 18:10:09 -0700

    Updated hex grid to trace out fiefdoms
    Signed-off-by: Woody NaDobhar <woodynadobhar@hotmail.com>

--- public/js/hexgridwidget.js
+++ public/js/hexgridwidget.js
@@ -52,8 +52,7 @@ $.fn.hexGridWidget = function (radius, columns, colMod, rows, rowMod, cssClass, 
 						toPoint(-1 * radius, 0)
 					].join(' '),
 					'class':cssClass + 
-						(data.hasOwnProperty(column+'-'+row) ? ' ' + data[column+'-'+row]['terrain'] : 'Undiscovered')
-						+ (data.hasOwnProperty(column+'-'+row) && data[column+'-'+row]['fiefdom'] ? ' fief' + data[column+'-'+row]['fiefdom']['id'].toString().split('').map(Number).reduce(function (a, b){return a + b;}, 0) : ''),
+						(data.hasOwnProperty(column+'-'+row) ? ' ' + data[column+'-'+row]['terrain'] : 'Undiscovered'),
 					tabindex:1,
 					'data-toggle':"tooltip",
 					'title': data[column+'-'+row]['name']
@@ -66,7 +65,11 @@ $.fn.hexGridWidget = function (radius, columns, colMod, rows, rowMod, cssClass, 
 				    mouseenter: hexHoverOn,
 				    mouseleave: hexHoverOff
 				})
-				.attr({'hex-row': row, 'hex-column': column});
+				.attr({
+					'hex-row': row, 
+					'hex-column': column, 
+					'fiefdom': data.hasOwnProperty(column+'-'+row) && data[column+'-'+row]['fiefdom'] ? data[column+'-'+row]['fiefdom']['id'] : ''
+				});
 
 				//cs
 				var left = center.x + 5;
@@ -78,5 +81,160 @@ $.fn.hexGridWidget = function (radius, columns, colMod, rows, rowMod, cssClass, 
 				}
 			}
 		}
+		
+		//draw svgs over fiefdoms
+		var fiefdoms = [];
+
+		//get all with fief != ''
+		$('#mapContainer').find("polygon[fiefdom!='']").each(function(){
+
+			//add to fiefdoms by id
+			if(typeof fiefdoms[$(this).attr('fiefdom')] == typeof undefined){
+				fiefdoms[$(this).attr('fiefdom')] = [];
+			}
+			
+			//add points
+			fiefdoms[$(this).attr('fiefdom')].push($(this).attr('points').split(" "));
+		});
+		
+		//iterate fiefdoms
+		for(var fiefdomKey in fiefdoms){
+			
+			//setup
+			var fifdomPoints = traceHex(fiefdoms[fiefdomKey]);
+			
+			//build polygon w/ points
+			createSVG('polygon').attr({
+				points: fifdomPoints.join(' '),
+				'class':'fiefdomBorder fiefdom' + fiefdomKey.toString().split('').map(Number).reduce(function (a, b){return a + b;}, 0),
+				tabindex:1,
+				'fiefdomId':fiefdomKey
+			})
+			.appendTo(svgParent);
+		};
 	});
 };
+
+function traceHex(fiefdom){
+	
+	//startup
+	points = [];
+	fiefKey = 0;
+	pointKey = 0;
+	
+	//determine last failed check point of first hex in fiefdom, if any
+	var tCheck = null;
+	for(var tPointKey in fiefdom[0]){
+		if(checkPoint(fiefdom[0][tPointKey], 0, fiefdom)){
+			tCheck = tPointKey;
+		}
+	}
+	
+	//complex area found
+	if(tCheck){
+		
+		//reorganize fiefdom[0] array so that tCheck1 is at point 0, and the order is maintainted
+		var nextPoint = (parseInt(tCheck) == 5) ? 0 : parseInt(tCheck) + 1;
+		var newStart = fiefdom[0][nextPoint];
+		while(fiefdom[0][0] != newStart){
+			fiefdom[0].push(fiefdom[0].shift());
+		}
+	}
+	
+	//do the things
+	fiefdomLoop(fiefdom);
+	
+	//init this mess
+	function fiefdomLoop(fiefdom){
+		
+		//setup
+		var firstRun = true;
+		
+		//loop thru fiefs
+		while(firstRun || (fiefKey != 0 && pointKey != 0)){
+			fiefLoop(fiefdom);
+			firstRun = false;
+		}
+	}
+	
+	//iterate a given fief's points
+	function fiefLoop(fiefdom){
+		
+		//setup
+		var doingFief = fiefKey;
+		
+		//loop thru points
+		while(doingFief == fiefKey && pointKey < fiefdom[fiefKey].length){
+			pointLoop(fiefdom);
+		}
+	}
+
+	//check points for collisions w/ other fiefs in fiefdom
+	function pointLoop(fiefdom){
+		
+		//check point against all other fief points
+		var check = checkPoint(fiefdom[fiefKey][pointKey], fiefKey, fiefdom);
+		
+		//if found in another hex
+		if(check){
+
+			//add it
+			points.push(fiefdom[fiefKey][pointKey]);
+			points.push(fiefdom[check[0]][check[1]]);
+			
+			//reorganize fiefdom[check[0]] array so that check1 is at point 0, and the order is maintainted
+			var newStart = fiefdom[check[0]][check[1]];
+			while(fiefdom[check[0]][0] != newStart){
+				fiefdom[check[0]].push(fiefdom[check[0]].shift());
+			}
+			
+			//update 'next' to that hex, second point
+			fiefKey = check[0];
+			pointKey = 1;
+
+		//if not
+		}else{
+			
+			//add it to final
+			points.push(fiefdom[fiefKey][pointKey]);
+			
+			//next
+			pointKey++;
+		}
+	}
+
+	function checkPoint(point, excludeFiefKey, fiefdom){
+		
+		//setup
+		var checkForPre = point.split(",");
+		var checkForA = checkForPre[0] + ',' + checkForPre[1];
+		var checkForB = checkForPre[0] + ',' + (parseInt(checkForPre[1]) + 1);
+		var checkForC = checkForPre[0] + ',' + (parseInt(checkForPre[1]) - 1);
+		var checkForD = (parseInt(checkForPre[0]) + 1) + ',' + checkForPre[1];
+		var checkForE = (parseInt(checkForPre[0]) - 1) + ',' + checkForPre[1];
+		
+		//iterate, excluding given Fief
+		for(var fiefKey in fiefdom){
+			if(fiefKey == excludeFiefKey){
+				continue;
+			}
+			
+			//see if x,y or x,y-1 or x,y+1 exists
+			for(var pointKey in fiefdom[fiefKey]){
+				if(fiefdom[fiefKey][pointKey] == checkForA || 
+				fiefdom[fiefKey][pointKey] == checkForB || 
+				fiefdom[fiefKey][pointKey] == checkForC || 
+				fiefdom[fiefKey][pointKey] == checkForD || 
+				fiefdom[fiefKey][pointKey] == checkForE){
+					return [
+					    fiefKey,
+					    pointKey
+					];
+				}
+			}
+		}
+		return false;
+	}
+	
+	return points;
+}
